73069466d43d78d15009d66a1e3211ef255521af
Martin Marusak <martin.marusak@protonmail.com>
2024-02-11 13:52:57 +0100

    Update

--- src/js/main-nav.js
+++ src/js/main-nav.js
@@ -20,3 +20,42 @@ menuBtn.addEventListener('click', () => {
     menuOpen = false;
   }
 });
+
+// ADDED
+
+// Close menu when clicking outside of it
+menu.addEventListener('click', event => {
+  if (!event.target.closest('.nav-menu')) {
+    // If clicked element is not inside the menu, close the menu
+    closeMenu();
+  }
+});
+
+// Close menu when clicking on any link inside the menu
+const menuLinks = document.querySelectorAll('.nav-menu a');
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    closeMenu();
+  });
+});
+
+function closeMenu() {
+  menu.classList.remove('active');
+  body.classList.remove('disable-scroll');
+  menuOpen = false;
+  menuBtn.classList.remove('open');
+}
+
+// END ADDED
+
+window.addEventListener('scroll', function () {
+  var container = document.getElementById('header-nav');
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 10) {
+    // Change background color after scrolling 100 pixels
+    container.classList.add('bg-white');
+  } else {
+    container.classList.remove('bg-white');
+  }
+});
